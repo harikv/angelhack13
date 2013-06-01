da45a237a8e8ad82226f88c96a9d505a45c1bdf1
harikv <nus.harikv@gmail.com>
2013-06-01 21:35:48 +0800

    Started work on Stack Model

--- stack/models.py
+++ stack/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class TechStack(models.model):
+	stack_item = models.ForeignKey(StackItem, null=True, default=None, blank=True)
+	#connection to kiran's model
+
+class StackItem(models.model):
+	item_tag = models.CharField(max_length=64, null=True, default=None, blank=True)
+	item_type = models.CharField(max_length=64, null=True, default=None, blank=True)
+	item_component = models.CharField(max_length=64, null=True, default=None, blank=True)
+	item_justification = models.CharField(max_length=1024, null=True, default=None, blank=True)
