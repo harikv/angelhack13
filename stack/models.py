2afb1e55fff4eb1fc9fedba3d64f58f17a27b3d1
Arnav Kumar <avi.arnav@gmail.com>
2013-06-02 11:12:35 +0800

    almost done

--- stack/models.py
+++ stack/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_init, pre_init, post_save, pre_save
+from django.template.defaultfilters import slugify
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -13,6 +16,7 @@ class StackItem(models.Model):
 
 class TechStack(models.Model):
 	domain = models.CharField(max_length=128, null=True, default=None, blank=True)
+	company_slug = models.SlugField(max_length=255, unique=True)
 
 	company_name = models.CharField(max_length=200, blank = True, null = True, verbose_name='Company Name')
 	company_description = models.CharField(max_length=1000, blank = True, null = True, verbose_name='Company Description')
@@ -38,3 +42,9 @@ class TechStack(models.Model):
 	server_hosting_provider = models.CharField(max_length=200, blank = True, null = True, verbose_name='Hosting Provider')
 	server_description = models.CharField(max_length=1000, blank = True, null = True, verbose_name='Why did you use these?')
 	#connection to kiran's model
+
+@receiver(post_save, sender=TechStack)
+def slugify_company_name(sender,instance,created,**kwargs):
+    if created:
+        instance.company_slug =  slugify(instance.company_name)
+        instance.save()
--- stack/views.py
+++ stack/views.py
@@ -2,18 +2,62 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.forms.models import inlineformset_factory
 from stack.models import StackItem, TechStack, ComponentList
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from stack.forms import TechStackForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+
 from django.template import RequestContext
+from django.views.generic import ListView, DetailView, TemplateView
 
 class LandingPageView(TemplateView):
     template_name = "landing.html"
 
-class CompanyPageView(TemplateView):
+class CompanyPageView(DetailView):
     template_name = "company.html"
+    model = TechStack
+    context_object_name = 'stack'
 
+    def get_object(self):
+        # Call the superclass
+        company_slug = self.kwargs['company_slug']
+        object = get_object_or_404(TechStack, company_slug=company_slug)
+        #object = TechStack.objects.get(id=1)
+
+        return object
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CompanyPageView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['company_name'] = self.object.company_name
+        context['company_description'] = self.object.company_description
+        context['company_type_of_application'] = self.object.company_type_of_application.split()
+
+        context['frontend_languages'] = self.object.frontend_languages.split()
+        context['frontend_framework'] = self.object.frontend_framework.split()
+        context['frontend_libraries'] = self.object.frontend_libraries.split()
+        context['frontend_description'] = self.object.frontend_description
+
+        context['backend_languages'] = self.object.frontend_libraries.split()
+        context['backend_framework'] = self.object.backend_framework.split()
+        context['backend_libraries'] = self.object.backend_libraries.split()
+        context['backend_description'] = self.object.backend_description
+
+        context['database'] = self.object.database.split()
+        context['database_description'] = self.object.database_description
+
+        context['server_os'] = self.object.server_os
+        context['server_configuration'] = self.object.server_configuration
+        context['server_ram'] = self.object.server_ram
+        context['server_nodes'] = self.object.server_nodes
+        context['server_hosting_provider'] = self.object.server_hosting_provider
+        context['server_description'] = self.object.server_description
+
+        return context
 
 
 def CreateStackForm(request):
@@ -21,7 +65,7 @@ def CreateStackForm(request):
         form = TechStackForm(request.POST)
         if form.is_valid():
             form.save()
-
+            return redirect(reverse('company_page', args=[form.instance.company_slug]))
     else:
         form = TechStackForm() # An unbound form
 
